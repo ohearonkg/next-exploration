1e008650ef6e7a8a56ada59be2ce43d113b1db81
K.G. O'Hearon <ohearonkg@gmail.com>
2018-11-04 20:54:49 -0400

    removing .next folder and adding styled component.s

--- components/CustomButton/CustomButton.tsx
+++ components/CustomButton/CustomButton.tsx
@@ -1,4 +1,10 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: blue;
+  color: white;
+`;
 
 export interface ButtonProps {
   /**
@@ -9,7 +15,7 @@ export interface ButtonProps {
 }
 
 const CustomButton: React.SFC<ButtonProps> = ({ text }) => (
-  <button> {text} </button>
+  <StyledButton> {text} </StyledButton>
 );
 
 export default CustomButton;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout';
+import Link from "next/link";
+import Layout from "../components/Layout";
+import CustomButton from "../components/CustomButton/CustomButton";
 
 export default () => (
   <Layout title="About | Next.js + TypeScript Example">
     <p>This is the about page</p>
-    <p><Link href='/'><a>Go home</a></Link></p>
+    <p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </p>
+    <CustomButton text="Hello" />
   </Layout>
-)
+);
